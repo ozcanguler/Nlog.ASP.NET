d8fdcaabb289cc465bae85ad200fe03a43c8d316
OzcanGuler <gulerozcan818@gmail.com>
2022-09-17 22:19:38 +0300

    restService_Simple

---
+++ WebApplication1/WcfService/HelloObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace WcfService
+{
+    public class HelloObject
+    {
+        [DataMember]
+        public bool happyHello { get; set; } = false;
+        [DataMember]
+        public string helloMessage { get; set; } = "Hey";
+    }
+}
---
+++ WebApplication1/WcfService/IService1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        // This file define the URLs that will be used in the REST service. In this example we will create 
+        // 1)Simple string response
+        // 2)URL that accepts a parameter number
+        // 3)a URL that will return a full object to the browser in JSON formatted text.
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "Hello/")]
+        string Hello();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetData/{Value}")]
+        string GetData(string value);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetModelObject/{id}")]
+        HelloObject GetModelObject(string id);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+       
+    }
+}
