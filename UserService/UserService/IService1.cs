fae5eeb3c83373d1e673b2ff379f166553dbf6f6
OzcanGuler <gulerozcan818@gmail.com>
2022-09-18 17:03:08 +0300

    Json Rest Service

---
+++ UserService/UserService/IService1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace UserService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json,UriTemplate = "/GetData/{value}")]
+        string GetData(string value);
+
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetAllUsers/")]
+        //List<User> GetAllUsers();
+        UserDTO GetAllUsers();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetUsersByName/{name}")]
+        //List<User> GetUsersByName(string name);
+        UserDTO GetUsersByName(string name);
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetUserById/{id}")]
+        //List<User> GetUserById(string id);
+        UserDTO GetUserById(string id);
+    }
+}
